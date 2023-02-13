57df45a3c5f1f7f69595c9e291c46dadfb898ae5
TristanLiao <tristan@17.media>
2023-02-13 18:29:39 +0800

    Create Comic18_iOS Xcode project

---
+++ Comic18_iOS/Comic18_iOS/Comic18_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Comic18_iOSApp.swift
+//  Comic18_iOS
+//
+//  Created by Bang Chiang Liao on 2023/2/13.
+//
+
+import SwiftUI
+
+@main
+struct Comic18_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Comic18_iOS/Comic18_iOSUITests/Comic18_iOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Comic18_iOSUITestsLaunchTests.swift
+//  Comic18_iOSUITests
+//
+//  Created by Bang Chiang Liao on 2023/2/13.
+//
+
+import XCTest
+
+final class Comic18_iOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
