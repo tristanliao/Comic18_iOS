c6622f7712ccb00aa7240cfe7d2d85365f70e9ea
TristanLiao <tristan@17.media>
2023-02-25 23:31:43 +0800

    Load comics through API

--- Comic18_iOS/Comic18_iOS/Comic18_iOSApp.swift
+++ Comic18_iOS/Comic18_iOS/Comic18_iOSApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Comic18_iOSApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainTabView()
         }
     }
 }
--- Comic18_iOS/Comic18_iOS/HomeView.swift
+++ Comic18_iOS/Comic18_iOS/HomeView.swift
@@ -8,13 +8,97 @@
 import SwiftUI
 
 struct HomeView: View {
+    let homeLoader = HomeLoader()
+    var comics: [Comic] = []
+    @State var recentComics: [Comic] = []
+    @State var latestKoreanComics: [Comic] = []
+    @State var recommendComics: [Comic] = []
+    @State var latestComics: [Comic] = []
+    let gridItems = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            Section("連載更新") {
+                LazyVGrid(columns: gridItems, spacing: 20) {
+                    ForEach(recentComics, id: \.self) { comic in
+                        ComicCellView(comic: comic)
+                    }
+                }
+            }
+            
+            Section("最新韓漫") {
+                LazyVGrid(columns: gridItems, spacing: 20) {
+                    ForEach(latestKoreanComics, id: \.self) { comic in
+                        ComicCellView(comic: comic)
+                    }
+                }
+            }
+            
+            Section("推薦") {
+                LazyVGrid(columns: gridItems, spacing: 20) {
+                    ForEach(recommendComics, id: \.self) { comic in
+                        ComicCellView(comic: comic)
+                    }
+                }
+            }
+            
+            Section("最新漫畫") {
+                LazyVGrid(columns: gridItems, spacing: 20) {
+                    ForEach(latestComics, id: \.self) { comic in
+                        ComicCellView(comic: comic)
+                    }
+                }
+            }
+        }
+        .onAppear(perform: loadComics)
+    }
+    
+    private func loadComics() {
+        homeLoader.loadRecentComics { result in
+            switch result {
+            case let .success(comics):
+                recentComics = comics
+            case let .failure(error):
+                print(error)
+            }
+        }
+        
+        homeLoader.loadLatestKoreanComics { result in
+            switch result {
+            case let .success(comics):
+                latestKoreanComics = comics
+            case let .failure(error):
+                print(error)
+            }
+        }
+        
+        homeLoader.loadRecommendComics { result in
+            switch result {
+            case let .success(comics):
+                recommendComics = comics
+            case let .failure(error):
+                print(error)
+            }
+        }
+        
+        homeLoader.loadLatestComics { result in
+            switch result {
+            case let .success(comics):
+                latestComics = comics
+            case let .failure(error):
+                print(error)
+            }
+        }
     }
 }
 
 struct HomeView_Previews: PreviewProvider {
+    
     static var previews: some View {
-        HomeView()
+        HomeView(comics: [
+            Comic.mockComic1,
+            Comic.mockComic2,
+            Comic.mockComic3
+        ])
     }
 }
