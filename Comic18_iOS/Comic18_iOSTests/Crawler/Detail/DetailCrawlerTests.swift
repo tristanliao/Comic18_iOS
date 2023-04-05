d88d550ada6a816b336e7f4c715ab79403ace187
TristanLiao <tristan@17.media>
2023-04-05 15:27:08 +0800

    Test `getDetailJSON` method in `DetailCrawler` with connection error case.

--- Comic18_iOS/Comic18_iOSTests/Crawler/Detail/DetailCrawlerTests.swift
+++ Comic18_iOS/Comic18_iOSTests/Crawler/Detail/DetailCrawlerTests.swift
@@ -6,30 +6,96 @@
 //
 
 import XCTest
+import SwiftSoup
+
+class DetailCrawler {
+    private enum Constants {
+        static let detailURL = "https://18comic.vip/album"
+    }
+    
+    enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+    
+    private let session: URLSession
+    let comicID: String
+    var detailHTML: String?
+    
+    init(comicID: String, session: URLSession = .shared) {
+        self.comicID = comicID
+        self.session = session
+    }
+    
+    private func getDetailHTML(completion: @escaping (Result<String, Error>) -> Void) {
+        if let detailHTML {
+            completion(.success(detailHTML))
+            return
+        }
+        
+        session.dataTask(with: URL(string: Constants.detailURL)!) { data, response, error in
+            if error != nil {
+                completion(.failure(.connectivity))
+            } else if let data = data, let html = String(data: data, encoding: .utf8) {
+                completion(.success(html))
+            } else {
+                completion(.failure(.invalidData))
+            }
+        }.resume()
+    }
+    
+    func getDetailJSON(completion: @escaping (Result<[String: Any], Error>) -> Void) {
+        getDetailHTML { result in
+            switch result {
+            case let .success(html):
+                completion(.success([:]))
+            case let.failure(error):
+                completion(.failure(.connectivity))
+            }
+        }
+    }
+}
 
 final class DetailCrawlerTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override func setUp() {
+        super.setUp()
+        URLProtocolStub.startInterceptingRequest()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        URLProtocolStub.stopInterceptionRequest()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func test_loadComicDetail_deliversError() {
+        let sut = makeSUT()
+        URLProtocolStub.stub(data: nil, response: nil, error: anyNSError)
+        
+        let exp = expectation(description: "Wait for getting detail json")
+        
+        sut.getDetailJSON { result in
+            switch result {
+            case let .failure(error):
+                XCTAssertEqual(error, .connectivity)
+            default:
+                XCTFail("Expect connectivity error, but get result")
+            }
+            
+            exp.fulfill()
         }
+        
+        wait(for: [exp], timeout: 5.0)
     }
 
+    // MARK: - Helpers
+    
+    private func makeSUT() -> DetailCrawler {
+        return DetailCrawler(comicID: "208122")
+    }
+    
+    private var anyNSError: NSError {
+        NSError(domain: "any error", code: 0)
+    }
+    
 }
